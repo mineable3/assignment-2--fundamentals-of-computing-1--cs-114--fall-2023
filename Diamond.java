9b44f3f5bbf4b506a757ba7cbc21759d3ab89d02
Emmet Spaeth <emmet.spaeth@gmail.com>
2023-10-16 15:49:22 -0400

    final cleaning of code

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,3 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Diamond {
@@ -10,14 +9,8 @@ public class Diamond {
     Scanner scan = new Scanner(System.in);
 
     System.out.println("Enter a number");
-
-    try {
-      diamondSize = scan.nextInt();
-      scan.close();
-    } catch (InputMismatchException ie) {
-      scan.close();
-      throw new InputMismatchException("You must enter an int");
-    }
+    diamondSize = scan.nextInt();
+    scan.close();
 
     if ((diamondSize % 2) == 0) {
       isEven = true;
@@ -39,6 +32,7 @@ public class Diamond {
           for (int numOfSpaces = (int)(diamondSize / 2) - row; numOfSpaces > 0; numOfSpaces--) {
             System.out.print("  ");
           }
+
           for (int numOfAstrisks = (2 * row); numOfAstrisks > 0; numOfAstrisks--) {
             System.out.print(" *");
           }
@@ -56,9 +50,10 @@ public class Diamond {
         //bottom half of the diamond
         if (row > (int)(diamondSize / 2)) {
           //padding
-          for (int numOfSpaces = (int)(-0.5 * diamondSize) + row; numOfSpaces > 0; numOfSpaces--) {
+          for (int numOfSpaces = (int)(-diamondSize / 2) + row; numOfSpaces > 0; numOfSpaces--) {
             System.out.print("  ");
           }
+
           for (int numOfAstrisks = (int)(2 * diamondSize) - (2 * row); numOfAstrisks > 0; numOfAstrisks--) {
             System.out.print(" *");
           }
@@ -73,6 +68,7 @@ public class Diamond {
       System.out.println("*");
 
     } else {
+
       for (int row = 0; row < diamondSize; row++) {
 
         //top half of the diamond
@@ -81,6 +77,7 @@ public class Diamond {
           for (int numOfSpaces = (int)((diamondSize) / 2) - row; numOfSpaces > 0; numOfSpaces--) {
             System.out.print(" ");
           }
+
           for (int numOfAstrisks = 1 + (2 * row); numOfAstrisks > 0; numOfAstrisks--) {
             System.out.print("*");
           }
@@ -98,9 +95,10 @@ public class Diamond {
         //bottom half of the diamond
         if (row > (int)(diamondSize / 2)) {
           //padding
-          for (int numOfSpaces = (int)(-0.5 * diamondSize) + row; numOfSpaces > 0; numOfSpaces--) {
+          for (int numOfSpaces = (int)(-diamondSize / 2) + row; numOfSpaces > 0; numOfSpaces--) {
             System.out.print(" ");
           }
+
           for (int numOfAstrisks = (int)(2 * diamondSize) - (2 * row) - 1; numOfAstrisks > 0; numOfAstrisks--) {
             System.out.print("*");
           }
