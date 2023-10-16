bea994d930af7e94ca8dcee8dea2e20f0d5fb925
Emmet Spaeth <emmet.spaeth@gmail.com>
2023-10-16 15:36:19 -0400

    adding comments

--- Diamond.java
+++ Diamond.java
@@ -25,6 +25,7 @@ public class Diamond {
 
     if (isEven) {
 
+      //starting astrisk
       for (int numOfSpaces = diamondSize; numOfSpaces > 0; numOfSpaces--) {
         System.out.print(" ");
       }
@@ -34,16 +35,13 @@ public class Diamond {
 
         //top half of the diamond
         if (row < (int)(diamondSize / 2)) {
-
           //padding
           for (int numOfSpaces = (int)(diamondSize / 2) - row; numOfSpaces > 0; numOfSpaces--) {
             System.out.print("  ");
           }
-
           for (int numOfAstrisks = (2 * row); numOfAstrisks > 0; numOfAstrisks--) {
             System.out.print(" *");
           }
-
           System.out.println();
         }
 
@@ -57,20 +55,18 @@ public class Diamond {
 
         //bottom half of the diamond
         if (row > (int)(diamondSize / 2)) {
-
           //padding
           for (int numOfSpaces = (int)(-0.5 * diamondSize) + row; numOfSpaces > 0; numOfSpaces--) {
             System.out.print("  ");
           }
-
           for (int numOfAstrisks = (int)(2 * diamondSize) - (2 * row); numOfAstrisks > 0; numOfAstrisks--) {
             System.out.print(" *");
           }
-
           System.out.println();
         }
       };
 
+      //ending astrisk
       for (int numOfSpaces = diamondSize; numOfSpaces > 0; numOfSpaces--) {
         System.out.print(" ");
       }
@@ -81,16 +77,13 @@ public class Diamond {
 
         //top half of the diamond
         if (row < (int)(diamondSize / 2)) {
-
           //padding
           for (int numOfSpaces = (int)((diamondSize) / 2) - row; numOfSpaces > 0; numOfSpaces--) {
             System.out.print(" ");
           }
-
           for (int numOfAstrisks = 1 + (2 * row); numOfAstrisks > 0; numOfAstrisks--) {
             System.out.print("*");
           }
-
           System.out.println();
         }
 
@@ -104,19 +97,15 @@ public class Diamond {
 
         //bottom half of the diamond
         if (row > (int)(diamondSize / 2)) {
-
           //padding
           for (int numOfSpaces = (int)(-0.5 * diamondSize) + row; numOfSpaces > 0; numOfSpaces--) {
             System.out.print(" ");
           }
-
           for (int numOfAstrisks = (int)(2 * diamondSize) - (2 * row) - 1; numOfAstrisks > 0; numOfAstrisks--) {
             System.out.print("*");
           }
-
           System.out.println();
         }
-
       }
     }
   }
